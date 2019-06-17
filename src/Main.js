33d1d38c96d07520264ca562f3ae1218194038e8
SlimTim10 <slimtim10@gmail.com>
2019-06-17 13:03:05 -0400

    Render some content based on hardcoded data

--- src/Main.js
+++ src/Main.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 class Main extends React.Component {
   render() {
+    const data = {
+      title: 'JavaScript Truthiness',
+      content: 'Falsy values: false, 0, null, undefined, NaN, empty string.\n\nTruthy values: everything else.',
+      author: 'Tim'
+    };
+
+    const post = (
+      <article className="post">
+        <header>
+          <p className="post-title">{data.title}</p>
+          <p className="post-author">{data.author}</p>
+        </header>
+        <pre className="post-content">{data.content}</pre>
+      </article>
+    );
+    
     return (
       <main className="main-content">
-        <p>Welcome!</p>
+        {post}
       </main>
     );
   }
